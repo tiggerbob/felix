0934427a01be3e3f34179f9b7996e37b4e9d928e
tiggerbob <tiggerbob@gmail.com>
2023-07-23 21:15:52 -0500

    Add timestamps to messages

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 
 import discord
 from discord.ext import commands
+from datetime import datetime, timedelta
 
 TOKEN = "MTEzMjE2MDM3OTMxNzkxOTc5Nw.GyFqpm.x5EOqIogKoJI0IWpHvl5TD2RjXMEDqPz4J1EsE"
 
@@ -12,8 +13,12 @@ bot = commands.Bot(command_prefix="!")
 embed = discord.Embed(title="Unread Messages", description="Here's what you missed", color=0x0000ff)
 tiggen_messages = ""
 loyl_messages = ""
+tiggen_time = ""
+loyl_time = ""
 # The person that sent the messages
 current_reader = ""
+
+UTC_TIME_DIFF = 7
 
 @bot.event
 async def on_ready():
@@ -21,7 +26,7 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-	global embed, tiggen_messages, loyl_messages, current_reader
+	global embed, tiggen_messages, loyl_messages, current_reader, tiggen_time, loyl_time, UTC_TIME_DIFF
 
 	await bot.process_commands(message)
 
@@ -30,36 +35,48 @@ async def on_message(message):
 
 	if message.content != "!delete" and message.content != "!inbox":
 		if message.author.id == 1131062164866744350:
-			# tiggen
+			# loyl
 			if current_reader == message.author.name:
 				loyl_messages += message.content + "\n"
+				timestamp = message.created_at + timedelta(hours=UTC_TIME_DIFF)
+				loyl_time += timestamp.strftime("%H:%M") + "\n"
 			else:
 				current_reader = message.author.name
 				embed = discord.Embed(title="Unread Messages", description="Here's what you missed", color=0x0000ff)
 				loyl_messages = message.content + "\n"
+				timestamp = message.created_at + timedelta(hours=UTC_TIME_DIFF)
+				loyl_time = timestamp.strftime("%H:%M") + "\n"
 				tiggen_messages = ""
+				tiggen_time = ""
 		elif message.author.id == 689281847469015130:
 			# tiggen
 			if current_reader == message.author.name:
 				tiggen_messages += message.content + "\n"
+				timestamp = message.created_at + timedelta(hours=UTC_TIME_DIFF)
+				tiggen_time += timestamp.strftime("%H:%M") + "\n"
 			else:
 				current_reader = message.author.name
 				embed = discord.Embed(title="Unread Messages", description="Here's what you missed", color=0x0000ff)
 				tiggen_messages = message.content + "\n"
+				timestamp = message.created_at + timedelta(hours=UTC_TIME_DIFF)
+				tiggen_time = timestamp.strftime("%H:%M") + "\n"
 				loyl_messages = ""
+				loyl_time = ""
 
 @bot.command(name="inbox")
 async def show_inbox(ctx):
-	global embed, tiggen_messages, loyl_messages
+	global embed, tiggen_messages, loyl_messages, tiggen_time, loyl_time
 	if ctx.message.author.id == 1131062164866744350:
 		# show tiggentime's messages
 		if not embed.fields:
-			embed.add_field(name="tiggen", value=tiggen_messages, inline=False)
+			embed.add_field(name="tiggen", value=tiggen_time, inline=True)
+			embed.add_field(name="\u200b", value=tiggen_messages, inline=True)
 		await ctx.send(embed=embed)
 	elif ctx.message.author.id == 689281847469015130:
 		# show loyl's messages
 		if not embed.fields:
-			embed.add_field(name="loyl", value=loyl_messages, inline=False)
+			embed.add_field(name="loyl", value=loyl_time, inline=True)
+			embed.add_field(name="\u200b", value=loyl_messages, inline=True)
 		await ctx.send(embed=embed)
 
 @bot.command(name="delete")
